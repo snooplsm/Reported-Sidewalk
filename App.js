05055ec660de59eebe9906223f5980c895b65bf3
adriennem76 <adrienne.c.miranda@gmail.com>
2019-09-21 13:19:16 -0400

    buttons showing

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./Home";
+import ComplaintType from "./ComplaintType"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home />
+      <ComplaintType />
     </View>
   );
 }
---
+++ ComplaintType.js
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import React from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import moment from "moment";
+
+
+export default class ComplaintType extends React.Component {
+  
+  render() {
+    const complaints = ["crossing", "curb", "sidewalk conditions", "sidewalk obstructions", "tree pit"]
+
+    return (
+      <View>
+        {complaints.map(complaint => (<Button title={complaint}/>))}
+      </View>
+    )
+  }
+}
