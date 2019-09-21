20a2b17031a391d1c5169c69b892268f4294fd23
Ryan Gravener <rgravener@moviepass.com>
2019-09-21 12:04:15 -0400

    add reverse geocode function

---
+++ Api.js
@@ -0,0 +1,25 @@
+export const reverseGeocode = location => {
+  console.log("reverse geocode", location);
+  if (!location || !location.lat || !location.lng) {
+    return Promise.reject(
+      `illegal location ${location == null ? null : JSON.stringify(location)}`
+    );
+  }
+  const key = `location.${location.lat}.${location.lng}`;
+  return AsyncStorage.getItem(key).then(item => {
+    if (item) {
+      console.log("found geo");
+      return JSON.parse(item);
+    }
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDiBYFqZLwPsNkMbRNqr1_63h-w9fcZNVM&latlng=${location.lat.toFixed(
+      4
+    )},${location.lng.toFixed(4)}&rankby=distance`;
+    return fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log("reverse geocoded!");
+        AsyncStorage.setItem(key, JSON.stringify(data));
+        return data;
+      });
+  });
+};
